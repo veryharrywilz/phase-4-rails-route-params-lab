55d1a14660799a6eab98da42c321887653222dfc
veryharrywilz <110489443+veryharrywilz@users.noreply.github.com>
2022-11-21 16:54:54 -0500

    commit tree 7a2eba16e30f0e7df4ecf67e93008ad79e40c80e

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+      students = if params[:name]
+                   Student.by_name(params[:name])
+                 else
+                   Student.all
+                 end    
     render json: students
   end
+
+  def by_id
+    render json: Student.find(params[:id])
+  end
+
+
 
 end
